b93359c9b493208d18fe8ea9dca7d6bcb62e1fa1
giriharan13 <lionking0304channel@gmail.com>
2023-09-19 23:53:02 +0530

    making some changes to the structure

--- FlaskApp/routes.py
+++ FlaskApp/routes.py
@@ -8,4 +8,36 @@ from CourseScrapper.CourseScrapper.spiders import UdemySpider,ClassCentralSpider
 from YoutubeVideoInfoGetter.YoutubeGetterater import YoutubeVideoInfoGetterater
 from WebsiteScrater.WebsiteScrater import WebsiteScrater
 
-# add the routes here
+from App import app
+from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+import random
+random_hex = ''.join(random.choice('0123456789ABCDEF') for _ in range(12))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # Note the correct URI format
+app.config["SECRET_KEY"] =random_hex
+db=SQLAlchemy(app)
+
+
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+@app.route('/student')
+def student():
+    return render_template('student.html')
+
+@app.route('/teacher')
+def teacher():
+    return render_template('teacher.html')
+
